22685439bd344665bdb38873876f1a78e6582c5a
CLARISSE\cfrem <c.fremez@yahoo.fr>
2019-03-25 15:00:30 +0100

    Create Patients.js

---
+++ src/Patients.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {Button} from 'reactstrap';
+
+class Pat extends Component
+{
+
+  constructor(props)
+  {
+    super(props);
+
+    this.state =
+    {
+      patients: []
+    }
+
+
+    fetch( "http://localhost:8000/api/v1/patients" , { method: "GET"})
+            .then((response) => {
+              return response.json();
+              })
+              .then((json) => {
+                this.setState({
+                  patients: json
+                })
+                });
+
+    }
+
+  
+
+
+
+  render() {
+    return (
+      <div className="Pat">
+        <header className="Pat-header">
+          <p>
+            Hello world
+          </p>
+          <Button color="danger">Retour</Button>
+          {
+            this.state.patients.map((pat) => <div>{pat.firstname}</div>)
+          }
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Pat;
