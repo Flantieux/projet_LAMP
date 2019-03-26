1b500a523795bc9ae52652046cb6f79d6d8da7a7
Aymeric du Pradel <aymericdupradel@gmail.com>
2019-03-26 13:27:54 +0100

    mise en page filtres et patients

--- projet_lamp/src/App.js
+++ projet_lamp/src/App.js
@@ -1,128 +1,73 @@
-import { BrowserRouter, Route, Link, NavLink as RRNavLink} from 'react-router-dom';
 import React, { Component } from 'react';
-//import logo from './fond.svg';
+import logo from './test.svg';
 import './App.css';
 import {Button} from 'reactstrap';
-import Filtres from './Filtres/filtres.js';
-import Pat from './Patients/Patients.js';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink,
-UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, ListGroup, ListGroupItem } from 'reactstrap';
 
+class App extends Component
+{
 
-class App extends Component {
-  constructor(props) {
+  constructor(props)
+  {
     super(props);
 
+    
+    this.state =
+    {
+      rooms: [],
+      rooms_vides: []
+    }
 
-    this.toggle = this.toggle.bind(this);
-    this.state = { patients: [] }
-    this.state = { isOpen : false };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-
-    /*fetch( "http://localhost:8000/api/v1/patients/" , { method: "GET" })
-            .then((response) => {
-              return response.json();
-              })
-              .then((json) => {
-                this.setState({
-                  patients: json
-                })
-              });
-            .then((json) => {
-              console.log(JSON.stringify(json));
-              });
-  //}
-  fetch( "http://localhost:8000/api/v1/patients/4" , { method: "PUT",
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'  
-        },
-        body: "room=5"
+    fetch("http://localhost:8000/api/v1/rooms", { method: "GET" })
+      .then((response) => {
+        return response.json();
       })
-      .then ((response) =>
-        {
-          return response.json();
+
+      .then((rooms) => {
+        let rooms_vides= [];
+        for (let room of rooms) {
+         // let nb_patients=0
+         // while(room.patients[nb_patients]!=="")
+         // {nb_patients=nb_patients+1;}
+         // if (room.capacity<nb_patients)
+         // {rooms_vides.push(room.id)}
+         rooms_vides.push("3")
         }
-      )
-      .then
-      (
-        (json) =>
-        {
-          this.update();
-          console.log(json);
-        }
-      )
-  };
+        //this.update();
+        this.setState({
+          rooms_vides: rooms_vides
+        });
+       }); 
+
+  }
+   
   
-  update() {
-      fetch( "http://localhost:8000/api/v1/patients" , { method: "GET"})
+ update()
+    {
+      fetch( "http://localhost:8000/api/v1/rooms" , { method: "GET"})
             .then((response) => {
               return response.json();
               })
               .then((json) => {
                 this.setState({
-                  patients: json
+                  rooms: json
                 })
                 });
-  }*/
 
-  /*constructor(props) {
-    super(props);
 
-    this.state = {
-      filters: []
-    }
-    fetch( "http://localhost:8000/api/v1/filters/" , { method: "GET" })
-            .then((response) => {
-              return response.json();
-              })
-              .then((json) => {
-                this.setState({
-                  filters: json
-                })
-                });*/
+              }
   
 
 
-
-
-  render() {
+  render()
+  {
     return (
       <div className="App">
-      <BrowserRouter>
-      
-      
-      
-
-        <Navbar color="light" light expand="md">
-          <NavLink tag={RRNavLink} exact to ="/App">Accueil</NavLink>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-
-              <NavItem>
-              <NavLink tag={RRNavLink} exact to="/patients">Patients</NavLink>
-              </NavItem>
-
-              <NavItem>
-              <NavLink tag={RRNavLink} exact to="/filters">Filtres</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-        <Route exact path="/patients" component={() => <Pat display={[]}onSend={()=>{}}/>}/>
-      <Route exact path="/filters" component={() => <Filtres display={[]}onSend={()=>{}}/>}/>
-      </BrowserRouter>
         <header className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Hello world
           </p>
-          
+          <Button color="danger">Coucou</Button>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -133,10 +78,8 @@ class App extends Component {
           </a>
           {
             //this.state.patients.map((pat) => <div>{pat.room}</div>)
+            this.state.rooms_vides.map((room) => <div>{room.id} </div>)
           }
-          {
-            //this.state.filters.map((pat) => <div>{pat.installation_date}</div>)
-          }*/}
         </header>
       </div>
     );
